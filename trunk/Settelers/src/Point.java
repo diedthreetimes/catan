3efab08a6b19d655a8a9bdb80a8e6e40c2b41108
pythonista <pythonista@48daeb94-82db-11de-bf8c-fd130f914ac9>
2009-08-13 02:24:25 +0000

    First two rows of board

--- trunk/Settelers/src/Point.java
+++ trunk/Settelers/src/Point.java
@@ -3,4 +3,10 @@ public class Point {
 	protected int x;
 	protected int y;
 	protected Resource [] resources;
+
+	private final int[] cards;
+
+	public Point(final int... cardTypes) {
+		this.cards = cardTypes;
+	}
 }
