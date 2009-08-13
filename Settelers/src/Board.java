fed6b423e0c86a65845f06eb80b214bd2ec2fb70
pythonista <pythonista@48daeb94-82db-11de-bf8c-fd130f914ac9>
2009-08-13 02:24:25 +0000

    First two rows of board

--- Settelers/src/Board.java
+++ Settelers/src/Board.java
@@ -1,7 +1,30 @@
-
 public class Board {
-	private Point [] spaces;
+	private Point[][] spaces = new Point[12][11];
 	
-	
-	
+	{
+		spaces[0] = new Point[] {
+			null,
+			null,
+			null,
+			new Point(Card.WOOD),
+			null,
+			new Point(Card.SHEEP),
+			null,
+			null, // Desert
+			null,
+			null,
+			null};
+		spaces[1] = new Point[] {
+			null,
+			null,
+			new Point(Card.WOOD),
+			null,
+			new Point(Card.WOOD, Card.SHEEP),
+			null,
+			new Point(Card.SHEEP),
+			null,
+			null, // Desert
+			null,
+			null};
+	}
 }
