8ab1c77c4069d984ad583a3a7328bbcc8b6bb07e
pythonista <pythonista@48daeb94-82db-11de-bf8c-fd130f914ac9>
2009-08-13 08:23:15 +0000

    Utility functions for the building phase

--- trunk/Settelers/src/Point.java
+++ trunk/Settelers/src/Point.java
@@ -1,18 +1,34 @@
+import java.util.*;
 
 public class Point {
-	protected int x;
-	protected int y;
-	protected Resource [] resources;
+    protected final int x;
+    protected final int y;
+    protected final Resource [] resources;
 
-	/**
-	 * Default constructor is needed for subclasses.
-	 */
-	public Point() {
-	}
-	
-	public Point(int x, int y, Resource... hexes){
-		 this.x = x;
-		 this.y = y;
-		 resources = hexes;
-	}
+    public Point(int x, int y, Resource... hexes){
+        this.x = x;
+        this.y = y;
+        resources = hexes;
+    }
+
+    /**
+     * NOTE: this logic explicitly assumes single player.
+     */
+    public boolean canPlaceSettlement(final Board b, final Collection<City> cities, final Collection<Settlement> settlements) {
+        for (final Point neighbor : b.getNeighbors(this)) {
+            for (final City city : cities) if (neighbor.equals(city)) {
+                return false;
+            }
+            for (final Settlement settlement : settlements) if (neighbor.equals(settlement)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(final Object o) {
+        return (o instanceof Point)
+            && (((Point)o).x == x)
+            && (((Point)o).y == y);
+    }
 }
