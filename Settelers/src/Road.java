a71d832f82ddb2fc5e564a514df987e3b8905163
pythonista <pythonista@48daeb94-82db-11de-bf8c-fd130f914ac9>
2009-08-13 08:23:15 +0000

    Utility functions for the building phase

--- Settelers/src/Road.java
+++ Settelers/src/Road.java
@@ -1,19 +1,28 @@
 import java.util.*;
 
 public class Road {
-	private Point start;
-	private Point end;
+    private Point start;
+    private Point end;
 
-	public Road ( Point start, Point end ){
-		this.start = start;
-		this.end = end;
-	}
+    public Road ( Point start, Point end ){
+        this.start = start;
+        this.end = end;
+    }
 
-	public Point getStart() {
-		return start;
-	}
+    public Point getStart() {
+        return start;
+    }
 
-	public Point getEnd() {
-		return end;
-	}
+    public Point getEnd() {
+        return end;
+    }
+
+    public Set<Point> getAllNeighbors(final Board b) {
+        final Set<Point> neighbors = new HashSet<Point>();
+        neighbors.addAll(b.getNeighbors(start));
+        neighbors.addAll(b.getNeighbors(end));
+        neighbors.remove(start);
+        neighbors.remove(end);
+        return neighbors;
+    }
 }
--- Settelers/src/Structure.java
+++ Settelers/src/Structure.java
@@ -2,27 +2,35 @@ import java.util.*;
 
 // enumerates structure names and costs
 public enum Structure {
-		ROAD {
-			boolean canBuild(final Map<Card.Type, Integer> hand) {
-				return hand.get(Card.Type.WOOD) >= 1 && hand.get(Card.Type.BRICK) >= 1;
-			}
-		},
-		SETTLEMENT {
-			boolean canBuild(final Map<Card.Type, Integer> hand) {
-				return hand.get(Card.Type.WOOD) >= 1 && hand.get(Card.Type.BRICK) >= 1
-					&& hand.get(Card.Type.WHEAT) >= 1 && hand.get(Card.Type.SHEEP) >= 1;
-			}
-		},
-		CITY {
-			boolean canBuild(final Map<Card.Type, Integer> hand) {
-				return hand.get(Card.Type.WHEAT) >= 2 && hand.get(Card.Type.ORE) >= 3;
-			}
-		};
+    ROAD {
+        boolean canBuild(final Map<Card.Type, Integer> hand) {
+            return hand.get(Card.Type.WOOD) >= 1 && hand.get(Card.Type.BRICK) >= 1;
+        }
+    },
+    SETTLEMENT {
+        boolean canBuild(final Map<Card.Type, Integer> hand) {
+            return hand.get(Card.Type.WOOD) >= 1 && hand.get(Card.Type.BRICK) >= 1
+                && hand.get(Card.Type.WHEAT) >= 1 && hand.get(Card.Type.SHEEP) >= 1;
+        }
+    },
+    CITY {
+        boolean canBuild(final Map<Card.Type, Integer> hand) {
+            return hand.get(Card.Type.WHEAT) >= 2 && hand.get(Card.Type.ORE) >= 3;
+        }
+    };
 
-		/**
-		 * @return true if given hand includes enough resources to build 
-		 * given structure
-		 */
-		abstract boolean canBuild(final Map<Card.Type, Integer> hand);
+    /**
+     * @return true if given hand includes enough resources to build 
+     * given structure
+     */
+    abstract boolean canBuild(final Map<Card.Type, Integer> hand);
+
+    public static Set<Structure> canBuildOptions(final Map<Card.Type, Integer> hand) {
+        final Set<Structure> options = EnumSet.noneOf(Structure.class); // awesomely efficient, backed by a bitmap!
+        for (final Structure s : Structure.values()) if (s.canBuild(hand)) {
+            options.add(s);
+        }
+        return options;
+    }
 }
 
